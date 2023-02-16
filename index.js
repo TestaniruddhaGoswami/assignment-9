235d0482bc6b6b99414858c4046257326d5929bf
Aniruddha Goswami <116855979+TestaniruddhaGoswami@users.noreply.github.com>
2023-02-16 04:32:53 +0000

    nothing much

---
+++ index.js
@@ -0,0 +1,95 @@
+// //problem -9
+// const a = Number(prompt("Enter first number: "));
+// const b = Number(prompt("Enter second number: "));
+// if(a,b==8||a+b==8||Math.abs(a-b)==8){console.log("true")}
+// else
+// {console.log("false")}
+
+
+//problem 8
+
+// program to find the largest among three numbers
+
+// const num1 = Number(prompt("Enter first number: "));
+// const num2 = Number(prompt("Enter second number: "));
+
+// const num3 = Number(prompt("Enter third number: "));
+// let largest;
+
+
+// if (num1 >= num2 && num1 >= num3) {
+//   largest = num1;
+// }
+// else if (num2 >= num1 && num2 >= num3) {
+//   largest = num2;
+// }
+// else {
+//   largest = num3;
+// }
+
+
+// console.log("The largest number is " + largest);
+
+
+//problem 7
+// var a = Number(prompt("enter the numbers only"));
+// if(isNaN(a)){console.log("no string,enter a number")}
+// else if(a>100&&a<200 || a>400&&a<500){console.log("true")}
+// else(console.log("false"))
+
+//problem 6
+// const a = Number(prompt("Enter first side: "));
+// const b = Number(prompt("Enter second side: "));
+
+// const c = Number(prompt("Enter third side: "));
+// let s = (a+b+c)/2;
+// let area =Math.sqrt(s*(s-a)*(s-b)*(s-c));
+// console.log(area);
+
+//problem 5
+// const year = Number(prompt("Enter the year--"));
+// if((year%4==0 && year%100!=0)||(year%400==0)){console.log("true");}
+// else {console.log("false");}
+
+//problem 4
+// var sum=0;var product=1;
+// for (i=10;i<=20;i++)
+// {
+
+//   sum=sum+i;
+
+//   product=product*i;
+
+// }
+// console.log(sum ,product);
+
+//
+
+//problem 2
+
+// const letters = ["Hi", "my", "name", "is", "Akash"];
+// var c="";
+// for (var i = 0; i < letters.length; i++) 
+// { 
+//   console.log(letters[i]);
+//      c =letters[i]+"" +c; 
+// }
+// console.log(c);
+
+//problem 1
+
+
+//  const a = Number(prompt("Enter first number: "));
+ 
+// if((a%2==0 )&& (a<15)){console.log("a")}
+// else if ((a%2==0)&& (a>15)){console.log("b")}
+// else if (a<10){console.log("c")}
+// else {console.log("d")}
+
+
+
+
+
+
+
+
